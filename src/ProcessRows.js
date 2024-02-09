edb956491d8aa9f33dbd6be020058840f89b39e2
Matt <matt@notablefi.com>
2024-02-09 16:17:13 -0600

    Refactor input file processing for Empower dashboard

--- src/ProcessRows.js
+++ src/ProcessRows.js
@@ -1,5 +1,5 @@
-import { mintCategories } from "./mintCategories";
-import { categoryMappings, fireCategories, creditNormalCategories } from "./categoryMappings";
+import { externalCategories } from "./externalCategories";
+import { categoryMappings, fireCategories } from "./categoryMappings";
 
 export default function ProcessRows(text, setDownload) {
     const delimiter = ',';
@@ -13,47 +13,42 @@ export default function ProcessRows(text, setDownload) {
 
     // set array of matched values
     // iterate through that array, for each category sum up the total and assign to a tuples of [category, summedAmount]
-    for (let i = 0; i < mintCategories.length; i++) {
+    for (let i = 0; i < externalCategories.length; i++) {
         let catTotal = 0.00;
         rows.forEach(row => {
-            const rowArr = row.replaceAll('ASF, DBA', 'ASF DBA').split(delimiter); // hacky replace to fix extra comma in Insperity rows
+            const rowArr = row.split(delimiter);
+            console.log(rowArr);
             let rowCat = 'No category found';
             if (rowArr[5]) {
-                rowCat = rowArr[5].replaceAll('"','');
+                rowCat = rowArr[3].replaceAll('\"','');
             }
-            if (rowCat === mintCategories[i]) {
-                if (rowArr[4] === '"credit"') {
-                    catTotal -= Number(rowArr[3].replaceAll('"', ''));
-                } else {
-                    catTotal += Number(rowArr[3].replaceAll('"', ''));
-                }
+            if (rowCat === externalCategories[i]) {
+                catTotal += Number(rowArr[5].replaceAll('"', ''));
             }
         });
-        mintSummed.push([mintCategories[i], catTotal.toFixed(2)]);
+        mintSummed.push([externalCategories[i], catTotal.toFixed(2)]);
     };
 
-    // for each FIRE category, sum up total of mint categories and assign to tuples of [category, total]
-    categoryMappings.forEach(cat => {
+    // for each FIRE category, sum up total of external categories and assign to tuples of [category, total]
+    categoryMappings.forEach(category => {
         let total = 0.00;
-        mintSummed.forEach(mintCat => {
-            if (mintCat[0] === cat[0]) {
-                total += Number(mintCat[1]);
+        mintSummed.forEach(extCategory => {
+            if (extCategory[0] === category[0]) {
+                total += Number(extCategory[1]);
             }
         });
-        if (fireSummed[cat[1]]) {
-            fireSummed[cat[1]] += total;
+        
+        if (fireSummed[category[1]]) {
+            fireSummed[category[1]] += total;
         } else {
-            fireSummed[cat[1]] = total;
+            fireSummed[category[1]] = total;
         }
     });
 
     // create csv export array in correct order
-    fireCategories.forEach(fireCat => {
-        const value = fireSummed[fireCat]?.toFixed(2);
-
-        // switch sign of credit-normal categories, such as Salary
-        const normalizedValue = creditNormalCategories.includes(fireCat) ? -value : value;
-        final.push([fireCat, normalizedValue]);
+    fireCategories.forEach(fireCategory => {
+        const value = Math.abs(fireSummed[fireCategory]?.toFixed(2) ?? 0.00);
+        final.push([fireCategory, value]);
     });
     
     setDownload(final);
